4b986cac2a1f6f86f30bec2bd2b73b6d572fe571
yy0931 <54441600+yy0931@users.noreply.github.com>
2023-01-06 11:03:44 +0900

    Update sqlite3_column_origin.c

--- sqlite3_column_origin.c
+++ sqlite3_column_origin.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-    if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+    if (argc == 2 && strcmp(argv[1], "--version") == 0) {
         fprintf(stderr, "1.0.0");  // semver
         return 1;
     }
